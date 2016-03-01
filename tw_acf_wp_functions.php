9a4ef3f80ede232e1221f853d17da241a8c57b7b
Mohamed Hamad <mohamed@thirdwunder.com>
2016-02-29 20:23:26 -0500

    fb on front_page

--- tw_acf_wp_functions.php
+++ tw_acf_wp_functions.php
@@ -2497,7 +2497,7 @@ add_action( 'wp_footer', 'tw_fb_script' );
 function tw_fb_script(){
   $fb_app_id = get_facebook_app_id();
   $fb_comments = tw_is_fb_coments_enabled();
-  if($fb_app_id && (is_singular('post') || is_front_page())):
+  if($fb_app_id && is_singular('post') || is_front_page()):
 ?>
   <div id="fb-root"></div>
   <script>(function(d, s, id) {
