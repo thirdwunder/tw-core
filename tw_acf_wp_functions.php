90fe6efdb8981d89c19722d8a6f41a5eb6f00aac
Mohamed Hamad <mohamed@thirdwunder.com>
2016-09-20 16:24:32 -0400

    removing owl from defer script

--- tw_acf_wp_functions.php
+++ tw_acf_wp_functions.php
@@ -14,6 +14,7 @@ if (!(is_admin() )) {
     if ( FALSE === strpos( $url, '.js' ) ) return $url;
     if ( strpos( $url, 'jquery.js' ) ) return $url;
     if ( strpos( $url, 'gravityforms' ) ) return $url;
+    if ( strpos( $url, 'owl' ) ) return $url;
 
     // return "$url' defer ";
     return "$url' defer onload='";
